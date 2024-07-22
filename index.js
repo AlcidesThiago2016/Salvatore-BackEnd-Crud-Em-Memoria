2d9cd07e64a8c7d46e89f04cf012d3c7c9216255
Alcides Thiago <alcides_thiago@hotmail.com>
2024-07-22 23:54:06 +0000

    Create EndPoint /personagem/_id

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ app.get('/personagem', function (req, res){
     res.send(lista)
 })
 
+// Endpoint Ready By ID [GET] /personagem/:id
+app.get('/personagem/:id', function(req, res) {
+    // Acessar o parametro ID
+    const id = req.params.id
+
+    // Acessar o item na lista usando o ID - 1
+    const item = lista[id - 1]
+
+    // Enviando o item com resposta
+    res.send(item)
+})
+
 app.listen(3000)
