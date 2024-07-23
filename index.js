ae535fb13dcd9c32ff0b793f7b2c09449e4611ce
Alcides Thiago <alcides_thiago@hotmail.com>
2024-07-23 13:22:02 +0000

    Implement create POST /personagem

--- index.js
+++ index.js
@@ -24,4 +24,22 @@ app.get('/personagem/:id', function(req, res) {
     res.send(item)
 })
 
+// Sinalizando para o express que utilizaremos JSON no Body
+app.use(express.json())
+
+// Endpoint Create (POST) /personagem
+app.post('/personagem', function (req , res) {
+    //Acessando o Body da Requisição
+    const body = req.body
+
+    //Acessando a propriedade 'nome' do body
+    const novoItem = body.nome
+
+    //Adicionando na lista
+    lista.push(novoItem)
+
+    //Exibindo uma mensagem de sucesso
+    res.send('Item adicionado com sucesso!' + novoItem)
+})
+
 app.listen(3000)
